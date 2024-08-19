f050009f9e57d75d84ef0665640f5819592be3e1
Emi <emma.lee.vtc@gmail.com>
2024-08-19 17:36:48 +0800

    new style new logo file

--- js/code.js
+++ js/code.js
@@ -1,5 +1,39 @@
 $( document ).ready(function() {
 
-  console.log( "ready!" );
+  // Variables
+  var body = $('body.page--home');
+  var animation_items = $('.animation--items');
+
+  // Functions
+  function begin_animations() {
+    animation_items.addClass('open');
+    body.addClass('animation--start');
+  }
+  function end_animations() {
+    setTimeout(() => {
+      body.addClass('animation--end');
+      // Reset Animation
+      setTimeout(() => {
+        animation_items.removeClass('open');
+        body.removeClass('animation--start animation--end');
+      }, 1000);
+    }, 250);
+  }
+
+  // Click - Work Panel Button
+  $(document).on('click', '.home--buttons button', function(e) {
+    // Begin Animation
+    // begin_animations();
+
+    // Change Page Content
+
+    // End Animation
+    // end_animations();
+  });
+
+  // // Click - Work Close Button
+  // $(document).on('click', 'button.work--close', function(e) {
+  //   console.log('close');
+  // });
   
 });
