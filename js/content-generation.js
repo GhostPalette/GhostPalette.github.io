85bee5edb6049cef3f0bc1c2814660aca1dcf707
Emi <emma.lee.vtc@gmail.com>
2025-02-14 00:07:38 +0800

    content-generation & grid

--- js/content-generation.js
+++ js/content-generation.js
@@ -7,7 +7,7 @@ async function populate() {
   const content = await response.json();
 
   populate_nav_work(content.work);
-  populate_content_work(content.work);
+  populate_content(content);
 }
 
 // --- Populate Nav: Work
@@ -32,34 +32,103 @@ function populate_nav_work(content) {
 }
 
 // --- Populate Content: Work
-function populate_content_work(content) {
-  console.log('populate content work function');
+function populate_content(content) {
+  // Grab Main Body
+  const main = document.querySelector("main");
 
+  // ----- WORK ----- //
+  // Loop Through JSON to Product Panels with Work
+  for( const work_item of content.work ) {
+    // Work Variables
+    const work_type = work_item.work_type;
+    const work_content = work_item.work_projects;
 
+    // Create Work Section & Header
+    const work_section = create_section_and_header(work_type);
+
+    // Create Work Items Element & Classes
+    const work_items_container = document.createElement("div");
+    work_items_container.classList.add("work--list");
+
+    for( const project of work_content ) {
+      // Item Variables
+      const project_cover = "https://ghostpalette.github.io/images/" + work_type + "/" + project.project_cover;
+      const project_label = project.project_label;
+      const project_items = project.project_items;
+
+      // Element Variables & Populate Classes + Attributes
+      const project_div = document.createElement("div");
+      project_div.classList.add("work--project");
+      const project_cover_lightbox = document.createElement("a");
+      project_cover_lightbox.setAttribute("href", project_cover);
+      project_cover_lightbox.setAttribute("data-lightbox", project.project_cover);
+      project_cover_lightbox.setAttribute("data-title", project_label);
+      const project_cover_image = document.createElement("img");
+      project_cover_image.setAttribute("src", project_cover);
+
+      // Nest Elements
+      project_cover_lightbox.appendChild(project_cover_image);
+      project_div.appendChild(project_cover_lightbox);
+      // Append Content
+      work_items_container.appendChild(project_div);
+    }
+
+    // Nest Work Items
+    work_section.appendChild(work_items_container);
+    // Append to Section to Content
+    main.appendChild(work_section);
+  }
+
+  // ----- PAGES ----- //
+  // Loop Through JSON to Product Panels with Work
+  for( const page of content.pages ) {
+    // Page Variables
+    const page_name = page.page_name;
+    const page_content = page.page_content;
+
+    // Create Work Section & Header
+    const page_section = create_section_and_header(page_name, 'page');
+
+    // Create Content Element
+    const page_content_div = document.createElement("div");
+    page_content_div.classList.add("page--content");
+    page_content_div.textContent = page_content;
+
+    // Nest Page Items
+    page_section.appendChild(page_content_div);
+    // Append to Section to Content
+    main.appendChild(page_section);
+  }
 
 }
 
+// ----- Functions ----- //
+function create_section_and_header( section_name, type = 'work' ) {
+  // Variables
+  const section_label = section_name.charAt(0).toUpperCase() + section_name.slice(1);;
 
-// --- Populate Content Panels
-// function populate_content(content) {
-//   // Grab Main Body
-//   const main = document.querySelector("main");
+  // Initialize Parent Elements & Their Classes
+  const item_section = document.createElement("section");
+  item_section.classList.add("container");
+  item_section.setAttribute('data_type', type);
+  item_section.setAttribute('data_' + type + '_type', section_name);
+  const item_header = document.createElement("div");
+  item_header.classList.add(type + "--header");
 
-//   // ----- WORK ----- //
-//   // Work Content
-//   const work_items = content.work;
-//   // Loop Through JSON to Product Panels with Work
-//   for( const work_item of work_items ) {
-//     // Work Variables
-//     const work_type = work_item.work_type;
-//     const work_content = work_item.work_items;
+  // Populate Header
+  const item_header_label = document.createElement("h2");
+  item_header_label.classList.add("label");
+  item_header_label.textContent = section_label;
 
-//     // Create Work Section & Header
-//     const work_section = create_section_and_header(work_type);
+  // Append Header Content
+  item_header.appendChild(item_header_label);
 
-//     // Create Work Items Element & Classes
-//     const work_items_container = document.createElement("div");
-//     work_items_container.classList.add("work--list");
+  // Append Items to Section
+  item_section.appendChild(item_header);
+
+  return item_section;
+}
+
 
 //     // Populate Work Items
 //     for( const work_item_content of work_content ) {
@@ -90,65 +159,6 @@ function populate_content_work(content) {
 //     // Append to Section to Content
 //     main.appendChild(work_section);
 //   }
-
-//   // ----- PAGES ----- //
-//   // Page Content
-//   const pages = content.pages;
-//   // Page Content Element
-//   const pages_div = document.querySelector("div.main--pages");
-//   // Loop Through JSON to Product Panels with Work
-//   for( const page of pages ) {
-//     // Page Variables
-//     const page_name = page.page_name;
-//     const page_content = page.page_content;
-
-//     // Create Work Section & Header
-//     const page_section = create_section_and_header(page_name);
-
-//     // Create Content Element
-//     const page_content_div = document.createElement("div");
-//     page_content_div.classList.add("page--content");
-//     page_content_div.textContent = page_content;
-
-//     // Nest Page Items
-//     page_section.appendChild(page_content_div);
-//     // Append to Section to Content
-//     pages_div.appendChild(page_section);
-//   }
-
-//   // ----- Functions ----- //
-//   function create_section_and_header( section_name ) {
-//     // Variables
-//     const section_label = section_name.charAt(0).toUpperCase() + section_name.slice(1);;
-
-//     // Initialize Parent Elements & Their Classes
-//     const item_section = document.createElement("section");
-//     item_section.classList.add("item--content");
-//     item_section.setAttribute('panel', section_name);
-//     const item_header = document.createElement("div");
-//     item_header.classList.add("item--header");
-
-//     // Populate Header
-//     const item_header_label = document.createElement("h3");
-//     item_header_label.classList.add("label");
-//     item_header_label.textContent = section_label;
-//     const item_header_button = document.createElement("button");
-//     item_header_button.classList.add("button--close");
-//     const item_header_button_icon = document.createElement("i");
-//     item_header_button_icon.classList.add("fa-solid", "fa-xmark");
-
-//     // Append Header Content
-//     item_header_button.appendChild(item_header_button_icon);
-//     item_header.appendChild(item_header_label);
-//     item_header.appendChild(item_header_button);
-
-//     // Append Items to Section
-//     item_section.appendChild(item_header);
-
-//     return item_section;
-//   }
-// }
-
 
 
 // --- Populate Footer with Buttons
