db7a0eda136ca5ad7365ee5f9b89d14d96a12c58
Emi <emma.lee.vtc@gmail.com>
2025-02-22 01:02:58 +0800

    v5

--- js/code.js
+++ js/code.js
@@ -1,71 +1,47 @@
 $( document ).ready(function() {
 
-  // // --- Variables
-  // var header = $('header');
-
-  // // --- Calculate Screen Proportions
-  // function calculate_screen_proportions() {
-  //   var body = $('body');
-  //   var window_width = $(window).width();
-  //   var window_height = $(window).height();
-  //   var window_ratio = window_width / window_height;
-
-  //   if( window_ratio < .5 ) {
-  //     body.attr('layout', 'horizontal');
-  //   } else if( window_ratio > 2 ) {
-  //     body.attr('layout', 'vertical');
-  //   } else {
-  //     body.attr('layout', 'normal');
-  //   }
-  // }
-  // calculate_screen_proportions();
-  // $(window).on('resize', function(e) {
-  //   calculate_screen_proportions();
-  // })
-
-  // // --- Click Work Button
-  // $(document).on('click', '.header--panel', function(e) {
-  //   var panel = $(this).attr('panel');
-  //   click_panel_button(panel);
-  //   header.addClass('close');
-  // });
-  // // --- Click Close Work Button
-  // $(document).on('click', '.main--work .button--close', function(e) {
-  //   header.removeClass('close');
-  //   setTimeout(function() {
-  //     $('.main--work, .main--work .item--content').removeClass('open');
-  //   }, 300);
-  // });
-
-  // // --- Click Page Button
-  // $(document).on('click', '.footer--menu .footer--button', function(e) {
-  //   var panel = $(this).attr('page');
-  //   click_panel_button(panel);
-  // });
-  // // --- Click Close Page Button
-  // $(document).on('click', '.main--pages .button--close', function(e) {
-  //   $('.main--pages, .main--pages .item--content').removeClass('open');
-  // });
-
-  // // --- Click Panel Button Functions
-  // function click_panel_button(panel) {
-  //   // Apply Opens
-  //   $('.item--content[panel="' + panel + '"]').parent().addClass('open');
-  //   $('.item--content[panel="' + panel + '"]').addClass('open');
-  // }
-
-  // // --- Click Footer Menu Button
-  // $(document).on('click', 'footer .footer--open', function(e) {
-  //   $(this).parent('footer').toggleClass('open');
-  // });
-
-
   // TODO:
-  // -- Footer buttons - functionality
-  // -- Responsive logo locations
+  //    Lightbox - Project internal galleries
+  //    Logo interaction animation
 
-  // For Fun:
-  // -- Logo interaction animation
-  // -- 
+  // --- Anchor Link - Smooth Scroll --- //
+  $(document).on('click', 'a[href^="#"]', function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+  });
+
+  // --- Project Menu - Button Active State --- //
+
+  // - Set Button to Active on Load
+  project_nav_button_active();
+  // - Check Section on Scroll - Set Button to Active
+  $(window).on('scroll', function(e) {
+    project_nav_button_active();
+  })
+  // - Function - Check Buttons to Set to Active
+  function project_nav_button_active() {
+    var workButtons = $('a.work--btn');
+    workButtons.each(function(e) {
+      var button = $(this);
+      var workType = button.attr('data_work_type');
+      var workSection = $('section[data_work_type="' + workType + '"');
+
+      var buttonTop = button.offset().top;
+      var buttonBottom = buttonTop + button.outerHeight();
+      var sectionTop = workSection.offset().top;
+      var sectionBottom = sectionTop + workSection.outerHeight();
+
+      if( buttonTop > sectionTop && buttonBottom < sectionBottom) {
+        button.addClass('active');
+      } else {
+        button.removeClass('active');
+      }
+    })
+  }
+
+
 
 });
--- js/content-generation.js
+++ js/content-generation.js
@@ -20,10 +20,10 @@ function populate_nav_work(content) {
     const work_type = work_item.work_type;
     const work_type_label = work_type.charAt(0).toUpperCase() + work_type.slice(1);
     // Create Elements
-    const my_work_button = document.createElement("button");
+    const my_work_button = document.createElement("a");
     // Populate Button Element
-    my_work_button.classList.add('work--btn');
-    // my_work_button.setAttribute('href', '#' + work_type);
+    my_work_button.classList.add('btn', 'work--btn');
+    my_work_button.setAttribute('href', '#' + work_type);
     my_work_button.setAttribute('data_work_type', work_type);
     my_work_button.textContent = work_type_label;
     // Append Button to Content
@@ -34,7 +34,7 @@ function populate_nav_work(content) {
 // --- Populate Content: Work
 function populate_content(content) {
   // Grab Main Body
-  const main = document.querySelector("main");
+  const main = document.querySelector("main .content--work");
 
   // ----- WORK ----- //
   // Loop Through JSON to Product Panels with Work
@@ -79,27 +79,6 @@ function populate_content(content) {
     main.appendChild(work_section);
   }
 
-  // ----- PAGES ----- //
-  // Loop Through JSON to Product Panels with Work
-  for( const page of content.pages ) {
-    // Page Variables
-    const page_name = page.page_name;
-    const page_content = page.page_content;
-
-    // Create Work Section & Header
-    const page_section = create_section_and_header(page_name, 'page');
-
-    // Create Content Element
-    const page_content_div = document.createElement("div");
-    page_content_div.classList.add("page--content", "container");
-    page_content_div.textContent = page_content;
-
-    // Nest Page Items
-    page_section.appendChild(page_content_div);
-    // Append to Section to Content
-    main.appendChild(page_section);
-  }
-
 }
 
 // ----- Functions ----- //
@@ -109,6 +88,7 @@ function create_section_and_header( section_name, type = 'work' ) {
 
   // Initialize Parent Elements & Their Classes
   const item_section = document.createElement("section");
+  item_section.setAttribute('id', section_name);
   item_section.setAttribute('data_type', type);
   item_section.setAttribute('data_' + type + '_type', section_name);
   const item_header = document.createElement("div");
@@ -119,6 +99,10 @@ function create_section_and_header( section_name, type = 'work' ) {
   item_header_label.classList.add("label");
   item_header_label.textContent = section_label;
 
+  if( type === 'work') {
+  item_header_label.classList.add("sr-only");
+  }
+
   // Append Header Content
   item_header.appendChild(item_header_label);
 
@@ -127,59 +111,6 @@ function create_section_and_header( section_name, type = 'work' ) {
 
   return item_section;
 }
-
-
-//     // Populate Work Items
-//     for( const work_item_content of work_content ) {
-//       // Item Variables
-//       const item_name = work_item_content.image_name;
-//       const item_src = "https://ghostpalette.github.io/images/" + work_type + "/" + work_item_content.image_src;
-//       const item_label = work_item_content.image_caption;
-
-//       // Element Variables & Populate Classes + Attributes
-//       const work_item_div = document.createElement("div");
-//       work_item_div.classList.add("work--item");
-//       const work_item_light_box = document.createElement("a");
-//       work_item_light_box.setAttribute("href", item_src);
-//       work_item_light_box.setAttribute("data-lightbox", work_type);
-//       work_item_light_box.setAttribute("data-title", item_label);
-//       const work_item_image = document.createElement("img");
-//       work_item_image.setAttribute("src", item_src);
-
-//       // Nest Elements
-//       work_item_light_box.appendChild(work_item_image);
-//       work_item_div.appendChild(work_item_light_box);
-//       // Append Content
-//       work_items_container.appendChild(work_item_div);
-//     }
-
-//     // Nest Work Items
-//     work_section.appendChild(work_items_container);
-//     // Append to Section to Content
-//     main.appendChild(work_section);
-//   }
-
-
-// --- Populate Footer with Buttons
-// function populate_footer_buttons(content) {
-//   // Grab Footer Item
-//   const footer_button_container = document.querySelector("footer .footer--menu");
-//   // Loop Through JSON to Produce Footer Buttons
-//   const pages = content.pages;
-//   for( const page of pages ) {
-//     // Pages Variables
-//     const page_name = page.page_name;
-//     const page_label = page_name.charAt(0).toUpperCase() + page_name.slice(1);
-//     // Create Elements
-//     const my_footer_button = document.createElement("button");
-//     // Populate Element
-//     my_footer_button.classList.add('footer--button');
-//     my_footer_button.setAttribute('page', page_name);
-//     my_footer_button.textContent = page_label;
-//     // Append Button to Content
-//     footer_button_container.appendChild(my_footer_button);
-//   }
-// }
 
 // --- Populate
 populate();
